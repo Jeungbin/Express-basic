ab671f7a739c86e0e143f70d37fed3a8626b283f
jeongbin <qls2467@gmail.com>
2022-07-25 23:05:39 -0600

    Change address system

--- main.js
+++ main.js
@@ -48,49 +48,12 @@ app.get("/", function (request, response) {
     
     `,
     //12. 정적인 파일의 서비스
-    `<a href="/create">create</a>`
+    `<a href="/topic/create">create</a>`
   );
   response.send(html);
 });
 
-//app.get(path , callback)
-app.get("/page/:pageId", function (request, response, next) {
-  var filteredId = path.parse(request.params.pageId).base;
-  // fs.readdir("./data", function (error, filelist) {
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    if (err) {
-      next(err);
-      // next('') 아무런 값이 없으면 다음 midware을 바로 실행
-      // err 는 err를 던짐
-    } else {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ["h1"],
-      });
-      var list = template.list(request.list);
-      //console.log(request.list);
-      //[ 'expreww' ]
-      var html = template.HTML(
-        sanitizedTitle,
-        list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="/delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    }
-  });
-});
-
-//HTML => req.params안에 들어가있음
-//pageId 를 통해서 {'pageId' :'HTML'}로 표현됨
-
-app.get("/create", function (request, response) {
+app.get("/topic/create", function (request, response) {
   //fs.readdir("./data", function (error, filelist) {
   var title = "WEB - create";
   var list = template.list(request.list);
@@ -98,7 +61,7 @@ app.get("/create", function (request, response) {
     title,
     list,
     `
-      <form action="/create_process" method="post">
+      <form action="/topic/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -113,6 +76,105 @@ app.get("/create", function (request, response) {
   response.send(html);
 });
 
+// this is body-parser
+app.post("/topic/create_process", function (request, response) {
+  //console.log(request.list);
+  //| undefined > 글 목록 data를 가져오지 않음
+  var post = request.body;
+  var title = post.title;
+  var description = post.description;
+  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+    response.redirect(`/topic/${title}`);
+  });
+});
+
+app.get("/topic/update/:pageId", function (request, response) {
+  var filteredId = path.parse(request.params.pageId).base;
+  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+    var title = request.params.pageId;
+    var list = template.list(request.list);
+    var html = template.HTML(
+      title,
+      list,
+      `
+        <form action="/topic/update_process" method="post">
+          <input type="hidden" name="id" value="${title}">
+          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
+          <p>
+            <textarea name="description" placeholder="description">${description}</textarea>
+          </p>
+          <p>
+            <input type="submit">
+          </p>
+        </form>
+        `,
+      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
+    );
+    response.send(html);
+  });
+});
+
+app.post("/topic/update_process", function (request, response) {
+  var post = request.body;
+  var id = post.id;
+  var title = post.title;
+  var description = post.description;
+  fs.rename(`data/${id}`, `data/${title}`, function (error) {
+    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
+      response.redirect(`/topic/${title}`);
+    });
+  });
+});
+
+app.post("/topic/delete_process", (request, response) => {
+  var post = request.body;
+  var id = post.id;
+  var filteredId = path.parse(id).base;
+  fs.unlink(`data/${filteredId}`, function (error) {
+    //response.writeHead(302, { Location: `/` });
+    //response.end();
+    response.redirect("/");
+  });
+});
+
+//app.get(path , callback)
+app.get("/topic/:pageId", function (request, response, next) {
+  var filteredId = path.parse(request.params.pageId).base;
+  // fs.readdir("./data", function (error, filelist) {
+  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
+    if (err) {
+      next(err);
+      // next('') 아무런 값이 없으면 다음 midware을 바로 실행
+      // err 는 err를 던짐
+      // 4개 인자를 가진 moddleware로 던져짐
+    } else {
+      var title = request.params.pageId;
+      var sanitizedTitle = sanitizeHtml(title);
+      var sanitizedDescription = sanitizeHtml(description, {
+        allowedTags: ["h1"],
+      });
+      var list = template.list(request.list);
+      //console.log(request.list);
+      //[ 'expreww' ]
+      var html = template.HTML(
+        sanitizedTitle,
+        list,
+        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+        ` <a href="/topic/create">create</a>
+          <a href="/topic/update/${sanitizedTitle}">update</a>
+          <form action="/topic/delete_process" method="post">
+            <input type="hidden" name="id" value="${sanitizedTitle}">
+            <input type="submit" value="delete">
+          </form>`
+      );
+      response.send(html);
+    }
+  });
+});
+
+//HTML => req.params안에 들어가있음
+//pageId 를 통해서 {'pageId' :'HTML'}로 표현됨
+
 /*var body = "";
   request.on("data", function (data) {
     body = body + data;
@@ -126,68 +188,6 @@ app.get("/create", function (request, response) {
       response.end();
     });
   });*/
-
-// this is body-parser
-app.post("/create_process", function (request, response) {
-  //console.log(request.list);
-  //| undefined > 글 목록 data를 가져오지 않음
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-    response.writeHead(302, { Location: `/?id=${title}` });
-    response.end();
-  });
-});
-
-app.get("/update/:pageId", function (request, response) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(
-      title,
-      list,
-      `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-      `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-app.post("/update_process", function (request, response) {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      response.redirect(`/?id=${title}`);
-    });
-  });
-});
-
-app.post("/delete_process", (request, response) => {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
-    //response.writeHead(302, { Location: `/` });
-    //response.end();
-    response.redirect("/");
-  });
-});
 
 app.use(function (req, res, next) {
   res.status(404).send("Sorry cant find that!!");
